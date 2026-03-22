65e7036335faed632706e20d4d7b79d77f2f2478
Morita Taro <38848223+taro0079@users.noreply.github.com>
2026-03-22 15:14:06 +0900

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://taro0079.github.io',
-    base: '/isogo-astro-page'
+  // site: 'https://taro0079.github.io',
+  // base: '/isogo-astro-page',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
