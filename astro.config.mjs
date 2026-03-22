78da08ae974e5956a4c828ac6fb2df7cb8681ba3
Morita Taro <38848223+taro0079@users.noreply.github.com>
2026-03-22 14:02:16 +0900

    first commint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://taro0079.github.io',
+    base: '/isogo-astro-page'
+});
---
+++ src/pages/page.md
@@ -0,0 +1,7 @@
+---
+layout: ../layouts/Layout.astro
+title: My First Markdown
+---
+
+# Tile
+This is my first page. written in **Markdown**.
